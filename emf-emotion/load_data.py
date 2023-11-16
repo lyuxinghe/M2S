cdd4a8d6561643eeced5030bda379a9e66f206df
Lyuxing He <840267395@qq.com>
2023-11-15 22:56:59 -0600

    data loading done

--- emf-emotion/load_data.py
+++ emf-emotion/load_data.py
@@ -3,6 +3,26 @@ import numpy as np
 import os
 import glob
 
+class Subject_Session_Data:
+        def __init__(self, f, file_count):
+                self.file_count = file_count
+                self.num_signals = f.signals_in_file
+                self.file_duration = f.file_duration
+                self.signal_labels = f.getSignalLabels()
+                self.data_dictionary = {}
+
+                for i in range(f.signals_in_file):
+                        data = f.readSignal(i)
+                        self.data_dictionary[self.signal_labels[i]] = data
+
+        def display_info(self):
+                print("file number:", self.file_count)
+                print("File duration in seconds:", self.file_duration)
+                print("Number of signals:", self.num_signals)
+                for label in self.signal_labels:
+                        print("Label:", label)
+                        print(self.data_dictionary[label])
+
 work_dir = os.path.dirname(os.path.dirname(os.getcwd()))
 dataset_dir = os.path.join(work_dir, "enterface06_EMOBRAIN", "Data")
 dataset_common_dir = os.path.join(dataset_dir, "Common")
@@ -18,10 +38,14 @@ pattern = os.path.join(dataset_EEG_dir, "*.bdf")
 bdf_files = glob.glob(pattern)
 
 count = 1
+data_list = []
 for file in bdf_files:
         # Open the BDF file
         try:
                 with pyedflib.EdfReader(file) as f:
+                        print("reading file :", count)
+                        data_list.append(Subject_Session_Data(f, count))
+                        '''
                         # Get general information
                         print("reading file :", count)
                         print("File duration in seconds:", f.file_duration)
@@ -33,6 +57,9 @@ for file in bdf_files:
                         for i in range(f.signals_in_file):
                                 data = f.readSignal(i)
                                 print(f"Data from signal {signal_labels[i]}:", data)
+                        '''
         except (OSError):
                 print("reading file :", count, "FAIL")
         count += 1
+
+data_list[0].display_info()
