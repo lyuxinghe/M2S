a2ca1e3ad0efdfb3d8313ef5cc60bed4ef9e0c9f
Lyuxing He <840267395@qq.com>
2023-12-03 13:43:57 -0600

    moved to ipynb

---
+++ emf-emotion/preprocess.py
@@ -0,0 +1,43 @@
+from scipy.signal import butter, lfilter
+
+# Bandpass filter
+# For: Environmental noise removal by applying a bandpass filter in the 4-45 Hz range
+# fs: sample rate, lowcut/ highcut: band cutoffs
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter(order, [lowcut, highcut], fs=fs, btype='band')
+    filtered = lfilter(b, a, data)
+    return filtered
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from scipy.signal import freqz
+
+    # Sample rate and desired cutoff frequencies (in Hz).
+    fs = 5000.0
+    lowcut = 500.0
+    highcut = 1250.0
+
+    # Filter a noisy signal.
+    T = 0.05
+    nsamples = T * fs
+    t = np.arange(0, nsamples) / fs
+    a = 0.02
+    f0 = 600.0
+    x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
+    x += 0.01 * np.cos(2 * np.pi * 312 * t + 0.1)
+    x += a * np.cos(2 * np.pi * f0 * t + .11)
+    x += 0.03 * np.cos(2 * np.pi * 2000 * t)
+    plt.figure(2)
+    plt.clf()
+    plt.plot(t, x, label='Noisy signal')
+
+    y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
+    plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
+    plt.xlabel('time (seconds)')
+    plt.hlines([-a, a], 0, T, linestyles='--')
+    plt.grid(True)
+    plt.axis('tight')
+    plt.legend(loc='upper left')
+
+    plt.show()
