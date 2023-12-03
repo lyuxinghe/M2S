e98c5518c5fac7b2fc7a273ffe6a98334f6e15e0
Lyuxing He <840267395@qq.com>
2023-12-03 16:39:49 -0600

    filter pushed

--- emf-emotion/preprocess.py
+++ emf-emotion/preprocess.py
@@ -4,7 +4,10 @@ from scipy.signal import butter, lfilter
 # For: Environmental noise removal by applying a bandpass filter in the 4-45 Hz range
 # fs: sample rate, lowcut/ highcut: band cutoffs
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter(order, [lowcut, highcut], fs=fs, btype='band')
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], fs=fs, btype='band')
     filtered = lfilter(b, a, data)
     return filtered
 
